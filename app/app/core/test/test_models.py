416d182566417d9c6ef835935ed81db4fd8c3270
“ArvidEllan” <ellanwambugu6@gmail.com>
2023-12-04 10:14:44 +0300

    changes in test models

--- app/app/core/test/test_models.py
+++ app/app/core/test/test_models.py
@@ -1,17 +1,15 @@
 
 
 from django.test import TestCase
-<<<<<<< HEAD
 from django.contrib.auth import get_user_model
-=======
 from django.contrib.auth import get_user_model 
 User = get_user_model()
->>>>>>> 50b77fd (updated changes)
+
 
 
 class ModelTest(TestCase):
     
-<<<<<<< HEAD
+
     def test_create_user_with_email_successful(self,username):
         """Test creating a new user with an email is successful"""
         email='test@example.com'
@@ -25,65 +23,59 @@ class ModelTest(TestCase):
         self.assertTrue(user.check_password(password))
         
     def test_new_user_email_normaized(self ):
-=======
-    def test_create_user_with_email_successful(self):
-        """Test creating a new user with an email is successful"""
-       # email='test@example.com'
-       # password='testpass2331'
-        self.user= User.objects.create_user(
-            email='test@example.com',
-            password='testpass124',
-            username = 'testname '
-    
-        )
-        self.assertEqual(self.user.email,'test@example.com')
-        self.assertTrue(self.user.check_password(password))
 
-    def test_new_user_email_normalized(self ):
->>>>>>> 50b77fd (updated changes)
-        sample_emails = [
-            ['test1@example.com', 'test1@example.com'],
-            ['Test2@example.com', 'test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'test3@example.com'],
-            
-        ]
-        for email , expected in sample_emails:
-<<<<<<< HEAD
-            user = get_user_model().objects.create_user(email, 'sample123')
-            self.assertEqual(user,email,expected)
-            
-=======
-            user = User.objects.create_user(email, 'sample123',)
-            
-            self.assertEqual(user,email,expected)
+        def test_create_user_with_email_successful(self):
+            """Test creating a new user with an email is successful"""
+        # email='test@example.com'
+        # password='testpass2331'
+            self.user= User.objects.create_user(
+                email='test@example.com',
+                password='testpass124',
+                username = 'testname '
+        
+            )
+            self.assertEqual(self.user.email,'test@example.com')
+            self.assertTrue(self.user.check_password(password))
+
+        def test_new_user_email_normalized(self ):
+
+            sample_emails = [
+                ['test1@example.com', 'test1@example.com'],
+                ['Test2@example.com', 'test2@example.com'],
+                ['TEST3@EXAMPLE.COM', 'test3@example.com'],
+                
+            ]
+            for email , expected in sample_emails:
+
+                user = get_user_model().objects.create_user(email, 'sample123')
+                self.assertEqual(user,email,expected)
+                
+
+                user = User.objects.create_user(email, 'sample123',)
+                
+                self.assertEqual(user,email,expected)
             
             
          
->>>>>>> 50b77fd (updated changes)
+
     def test_new_user_without_email_raises_error(self):
         '''Test that creating user without email raises error'''
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None,'test1234567890')
     
-<<<<<<< HEAD
+
     def test_create_new_superuser(self,username ):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
             'test@londonappdev.com',
-            'test123'
-=======
+            'test123')
+
     def test_create_new_superuser(self):
         user = get_user_model().objects.create_superuser(
             'test@londonappdev.com',
             'test123'
-            
->>>>>>> 50b77fd (updated changes)
         )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-<<<<<<< HEAD
-                
-=======
-                
->>>>>>> 50b77fd (updated changes)
+
